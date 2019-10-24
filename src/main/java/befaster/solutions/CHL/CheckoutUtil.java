8520d2e50a227d7be49b4cb1b85c779fec6b80e1
sbx_user1051 <sbx_user1051@169.254.14.101>
2019-10-24 20:26:05 +0000

    Thu Oct 24 20:26:05 UTC 2019

--- src/main/java/befaster/solutions/CHL/CheckoutUtil.java
+++ src/main/java/befaster/solutions/CHL/CheckoutUtil.java
@@ -1,5 +1,8 @@
 package befaster.solutions.CHL;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class CheckoutUtil {
 
     private Result getResult(Integer total, Integer offer) {
@@ -8,9 +11,27 @@ class CheckoutUtil {
 
     public Integer getTotalPrice(String skus){
 
+
+    }
+
+
+    private Map<String,Integer> getSkuCountMap(String skus){
+
+        Map<String,Integer> priceList = new HashMap<String,Integer>(){
+            {
+                put("A",50);
+                put("B",30);
+                put("C",20);
+                put("D",15);
+            }
+        };
+        String[] items = skus.split("");
+        //A,B,C,D,A
+        for(String thisItem: items){
+
+        }
+
     }
 
 
 }
-
-
--- src/main/java/befaster/solutions/CHL/PriceService.java
+++ src/main/java/befaster/solutions/CHL/PriceService.java
@@ -19,4 +19,3 @@ public class PriceService {
         return priceList;
     }
 }
-
