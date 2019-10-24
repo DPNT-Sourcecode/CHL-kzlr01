4c26296b2c7e8b11e55d8d82d24e2e7ecc98e20c
sbx_user1051 <sbx_user1051@169.254.14.101>
2019-10-24 19:41:58 +0000

    Thu Oct 24 19:41:58 UTC 2019

---
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -0,0 +1,16 @@
+package befaster.solutions.HLO;
+
+import befaster.solutions.SUM.SumSolution;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class HelloSolutionTest {
+    private SumSolution sumSolution;
+
+    @Before
+    public void setUp() {
+        sumSolution= new SumSolution();
+    }
+
+}
