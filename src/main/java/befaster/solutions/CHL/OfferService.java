e6c9203674e3dac5bf9c4e7fa0e6c60879208a5a
sbx_user1051 <sbx_user1051@169.254.14.101>
2019-10-24 21:04:56 +0000

    Thu Oct 24 21:04:56 UTC 2019

--- src/main/java/befaster/solutions/CHL/OfferService.java
+++ src/main/java/befaster/solutions/CHL/OfferService.java
@@ -9,6 +9,7 @@ public class OfferService {
     //Ideally from a persistance
     private Set<Offer> currentOffers = new HashSet<Offer>(){
         {
+            add(new Offer("A",5, 130));
             add(new Offer("A",3, 130));
             add(new Offer("B",2, 45));
         }
@@ -23,6 +24,7 @@ public class OfferService {
     }
 
 }
+
 
 
 
