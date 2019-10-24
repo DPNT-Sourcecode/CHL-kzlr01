6903c3def3231c12307b402ce115ae7d59f9168b
sbx_user1051 <sbx_user1051@169.254.14.101>
2019-10-24 19:59:05 +0000

    Thu Oct 24 19:59:05 UTC 2019

--- src/test/java/befaster/solutions/CHL/CheckliteSolutionTest.java
+++ src/test/java/befaster/solutions/CHL/CheckliteSolutionTest.java
@@ -4,4 +4,20 @@ import static org.junit.Assert.*;
 
 public class CheckliteSolutionTest {
 
+    private String emptySkUs = "";
+    private String oneSku = "A";
+    private String noOfferSKUs = "ADE";
+    private String sKUsWithOffer = "ADEA";
+
+    private SumSolution sumSolution;
+
+    @Before
+    public void setUp() {
+        sumSolution= new SumSolution();
+    }
+
+    @Test(expected = InvalidComputeParamException.class)
+    public void whenTheFirstParamisNegative_ItShouldReturnNull() {
+        assertThat( sumSolution.sum(-1,0), equalTo(null));
+    }
 }
