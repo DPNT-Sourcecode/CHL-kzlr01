81d25f66aa0730610a8befa441af665c71a6a61c
sbx_user1051 <sbx_user1051@169.254.14.101>
2019-10-24 19:44:36 +0000

    Thu Oct 24 19:44:36 UTC 2019

--- src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -1,16 +1,34 @@
 package befaster.solutions.HLO;
 
-import befaster.solutions.SUM.SumSolution;
+import befaster.solutions.exceptions.InvalidComputeParamException;
 import org.junit.Before;
+import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.*;
 
 public class HelloSolutionTest {
-    private SumSolution sumSolution;
+    private HelloSolution helloSolution;
 
     @Before
     public void setUp() {
-        sumSolution= new SumSolution();
+        helloSolution= new HelloSolution();
     }
 
+
+    @Test
+    public void returnEmptyWhenEmptyParam() {
+        assertThat( helloSolution.hello(""), equalTo(""));
+    }
+
+    @Test
+    public void returnMessageWithAHelloPrefix() {
+        assertThat( helloSolution.hello("testname"), equalTo("Hello testname"));
+    }
+
+    @Test
+    public void returnMessageWithAHelloPrefixEvenWithNewLineChar() {
+        assertThat( helloSolution.hello("\ntestname"), equalTo("Hello \ntestname"));
+    }
 }
