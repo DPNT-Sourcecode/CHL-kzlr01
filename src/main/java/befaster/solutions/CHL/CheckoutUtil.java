ab4d9b5992ac95e406bab98ad0dff8c4e5165244
sbx_user1051 <sbx_user1051@169.254.14.101>
2019-10-24 20:55:52 +0000

    Thu Oct 24 20:55:52 UTC 2019

--- src/main/java/befaster/solutions/CHL/CheckoutUtil.java
+++ src/main/java/befaster/solutions/CHL/CheckoutUtil.java
@@ -2,6 +2,7 @@ package befaster.solutions.CHL;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 class CheckoutUtil {
@@ -11,41 +12,47 @@ class CheckoutUtil {
     private OfferService offerService = new OfferService();
 
 
-    Integer getTotalPrice(String skus){
+    Integer getTotalPrice(String skus) {
+
+        Integer grandTotal = 0;
 
         Map<String, Integer> priceList = priceService.getPriceList();
-        Set<Offer> currentOffer = offerService.getCurrentOffer();
 
-        Map<String,Integer> itemWiseCount = getSkuCountMap(skus);
+        Map<String, Integer> itemWiseCount = getSkuCountMap(skus);
 
-        for(String thisSku: itemWiseCount.keySet()){
+        for (String thisSku : itemWiseCount.keySet()) {
+            Integer thisLineValue = 0;
+            Optional<Offer> maybeOffer = offerService.getOfferFor(thisSku);
 
-            offerService.getCurrentOffer(thisSku)
+            if (maybeOffer.isPresent()) {
+                Offer thisOffer = maybeOffer.get();
+                Result result = getResult(itemWiseCount.get(thisSku), thisOffer.getOfferCount());
+                thisLineValue = (result.getOfferCount() * thisOffer.getOfferPrice()) + (result.getRemainingCount() * priceList.get(thisSku));
+            } else {
+                thisLineValue = itemWiseCount.get(thisSku) * priceList.get(thisSku);
+            }
 
+            grandTotal = grandTotal + thisLineValue;
         }
-
-
-
-
-
+        return grandTotal;
     }
+
 
     private Result getResult(Integer total, Integer offer) {
         return new Result(total / offer, total % offer);
     }
 
 
+    private Map<String, Integer> getSkuCountMap(String skus) {
 
-    private Map<String,Integer> getSkuCountMap(String skus){
-
-        Map<String,Integer> result = new HashMap<>();
+        Map<String, Integer> result = new HashMap<>();
 
         String[] items = skus.split("");
         //A,B,C,D,A
-        for(String thisSku: items){
-            if(result.containsKey(thisSku)){
+        for (String thisSku : items) {
+            if (result.containsKey(thisSku)) {
                 result.put(thisSku, result.get(thisSku) + 1);
-            }else{
+            } else {
                 result.put(thisSku, 1);
             }
         }
@@ -54,7 +61,3 @@ class CheckoutUtil {
 
 
 }
-
-
-
-
