c15d0e82e627bf2a3c9ac94c57f8fcafd8c67759
sbx_user1051 <sbx_user1051@169.254.14.101>
2019-10-24 19:18:50 +0000

    Thu Oct 24 19:18:50 UTC 2019

--- src/main/java/befaster/SendCommandToServer.java
+++ src/main/java/befaster/SendCommandToServer.java
@@ -53,23 +53,28 @@ public class SendCommandToServer {
      *
      **/
     public static void main(String[] args) throws ConfigNotFoundException, InvalidComputeParamException {
-        EntryPointMapping entry = new EntryPointMapping();
+        try {
+            EntryPointMapping entry = new EntryPointMapping();
 
-        QueueBasedImplementationRunner runner = new QueueBasedImplementationRunner.Builder()
-                .setConfig(getRunnerConfig())
-                .withSolutionFor("sum", entry::sum)
-                .withSolutionFor("hello", entry::hello)
-                .withSolutionFor("array_sum", entry::arraySum)
-                .withSolutionFor("int_range", entry::intRange)
-                .withSolutionFor("fizz_buzz", entry::fizzBuzz)
-                .withSolutionFor("checkout", entry::checkout)
-                .withSolutionFor("checklite", entry::checklite)
-                .create();
+            QueueBasedImplementationRunner runner = new QueueBasedImplementationRunner.Builder()
+                    .setConfig(getRunnerConfig())
+                    .withSolutionFor("sum", entry::sum)
+                    .withSolutionFor("hello", entry::hello)
+                    .withSolutionFor("array_sum", entry::arraySum)
+                    .withSolutionFor("int_range", entry::intRange)
+                    .withSolutionFor("fizz_buzz", entry::fizzBuzz)
+                    .withSolutionFor("checkout", entry::checkout)
+                    .withSolutionFor("checklite", entry::checklite)
+                    .create();
 
-        ChallengeSession.forRunner(runner)
-                .withConfig(getConfig())
-                .withActionProvider(new UserInputAction(args))
-                .start();
+            ChallengeSession.forRunner(runner)
+                    .withConfig(getConfig())
+                    .withActionProvider(new UserInputAction(args))
+                    .start();
+        }catch (InvalidComputeParamException){
+            //
+        }
     }
 
 }
+
