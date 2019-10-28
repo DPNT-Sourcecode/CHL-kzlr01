5139f6e354ef32ae4bcfa5bf743040f237bc9290
sbx_user1051 <sbx_user1051@169.254.21.5>
2019-10-28 20:34:05 +0000

    Mon Oct 28 20:34:05 UTC 2019

--- src/main/java/befaster/solutions/CHL/BuyGetOffer.java
+++ src/main/java/befaster/solutions/CHL/BuyGetOffer.java
@@ -1,4 +1,50 @@
 package befaster.solutions.CHL;
 
 public class BuyGetOffer {
+
+    private String buySku;
+    private Integer buyCount;
+
+    private String freeSku;
+    private Integer freeCount;
+
+    public BuyGetOffer(String buySku, Integer buyCount, String freeSku, Integer freeCount) {
+        this.buySku = buySku;
+        this.buyCount = buyCount;
+        this.freeSku = freeSku;
+        this.freeCount = freeCount;
+    }
+
+    public String getBuySku() {
+        return buySku;
+    }
+
+    public void setBuySku(String buySku) {
+        this.buySku = buySku;
+    }
+
+    public Integer getBuyCount() {
+        return buyCount;
+    }
+
+    public void setBuyCount(Integer buyCount) {
+        this.buyCount = buyCount;
+    }
+
+    public String getFreeSku() {
+        return freeSku;
+    }
+
+    public void setFreeSku(String freeSku) {
+        this.freeSku = freeSku;
+    }
+
+    public Integer getFreeCount() {
+        return freeCount;
+    }
+
+    public void setFreeCount(Integer freeCount) {
+        this.freeCount = freeCount;
+    }
 }
+
--- src/main/java/befaster/solutions/CHL/OfferService.java
+++ src/main/java/befaster/solutions/CHL/OfferService.java
@@ -13,13 +13,29 @@ public class OfferService {
             add(new CountOffer("B",2, 45));
         }
     };
+    //Ideally from a persistance
+    private List<CountOffer> buyGetOffers = new ArrayList<Bu>(){
+        {
+            add(new CountOffer("A",5, 200));
+            add(new CountOffer("A",3, 130));
+            add(new CountOffer("B",2, 45));
+        }
+    };
 
 
     public List<CountOffer> getCountOffersFor(String sku){
         return countOffers.stream().filter(o -> o.getSku().equals(sku)).collect(Collectors.toList());
     }
 
+
+    public Map<String,Integer> getFreeItems( Map<String, Integer> orderedItems){
+
+
+
+    }
+
 }
+
 
 
 
