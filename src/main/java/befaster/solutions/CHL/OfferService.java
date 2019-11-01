0ee0cf649fd7333b697639f386228ad37f7484b7
sbx_user1051 <sbx_user1051@169.254.46.13>
2019-11-01 07:08:34 +0000

    Fri Nov 01 07:08:34 UTC 2019

--- src/main/java/befaster/solutions/CHL/OfferService.java
+++ src/main/java/befaster/solutions/CHL/OfferService.java
@@ -25,13 +25,20 @@ public class OfferService {
     };
     */
 
-
     //Ideally from a persistance or injected repo which can be mocked in the unit test
     private List<CountOffer> countOffers = new ArrayList<CountOffer>() {
         {
-            add(new CountOffer("A", 5, 200));
             add(new CountOffer("A", 3, 130));
+            add(new CountOffer("A", 5, 200));
             add(new CountOffer("B", 2, 45));
+            add(new CountOffer("H", 5, 45));
+            add(new CountOffer("H", 10, 80));
+
+            add(new CountOffer("K", 2, 150));
+            add(new CountOffer("P", 5, 200));
+            add(new CountOffer("Q", 3, 80));
+            add(new CountOffer("V", 2, 90));
+            add(new CountOffer("V", 3, 130));
         }
     };
 
@@ -40,6 +47,9 @@ public class OfferService {
         {
             add(new BuyGetOffer("E", 2, "B", 1));
             add(new BuyGetOffer("F", 2, "F", 1));
+            add(new BuyGetOffer("N", 3, "M", 1));
+            add(new BuyGetOffer("R", 3, "Q", 1));
+            add(new BuyGetOffer("U", 3, "U", 1));
         }
     };
 
@@ -52,6 +62,7 @@ public class OfferService {
     }
 
 }
+
 
 
 
